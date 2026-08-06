0e02a55d0d8742aaa9af431807b241dc6e057cce
alifurkansagir <alifurkansagir69@gmail.com>
2026-08-07 02:15:59 +0300

    delete help_printer function in utils.cpp file

--- sources/utils/Utils.cpp
+++ sources/utils/Utils.cpp
@@ -20,17 +20,3 @@ void Server::clearEmptyChannels() {
         }
     }
 }
-
-void Server::help_printer(std::string target, int fd) {
-    std::string botName = "Helper";
-    std::string helpText = "Commands that you can use : !help, !ping, !rules";
-    std::string botMsg = ":" + botName + " PRIVMSG " + target + " :" + helpText;
-    
-    if (target[0] == '#') {
-        if (_channels.find(target) != _channels.end()) {
-            _channels[target]->broadcast(botMsg, NULL);
-        }
-    } else {
-        sendMessage(fd, botMsg);
-    }
-}
